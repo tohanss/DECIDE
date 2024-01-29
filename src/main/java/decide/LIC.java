86cb2ab14517eb5b36650a80c29a0a2b390e1eba
jsoderholm <103559938+jsoderholm@users.noreply.github.com>
2024-01-29 09:45:38 +0100

    Initial Setup: Maven Project Structure and Basic Code Skeletons (#23)
    * Add Basic Folder Structure & Code Skeletons
    
    * Add .gitignore
    
    * Add Test Class Skeletons
    
    * Closes #2

---
+++ src/main/java/decide/LIC.java
@@ -0,0 +1,85 @@
+package decide;
+
+public class LIC {
+    private boolean LIC_0() {
+        return false;
+    }
+
+    private boolean LIC_1() {
+        return false;
+    }
+
+    private boolean LIC_2() {
+        return false;
+    }
+
+    private boolean LIC_3() {
+        return false;
+    }
+
+    private boolean LIC_4() {
+        return false;
+    }
+
+    private boolean LIC_5() {
+        return false;
+    }
+
+    private boolean LIC_6() {
+        return false;
+    }
+
+    private boolean LIC_7() {
+        return false;
+    }
+
+    private boolean LIC_8() {
+        return false;
+    }
+
+    private boolean LIC_9() {
+        return false;
+    }
+
+    private boolean LIC_10() {
+        return false;
+    }
+
+    private boolean LIC_11() {
+        return false;
+    }
+
+    private boolean LIC_12() {
+        return false;
+    }
+
+    private boolean LIC_13() {
+        return false;
+    }
+
+    private boolean LIC_14() {
+        return false;
+    }
+
+    public boolean[] calculateCMV() {
+        boolean[] CMV = new boolean[15];
+
+        CMV[0] = LIC_0();
+        CMV[1] = LIC_1();
+        CMV[2] = LIC_2();
+        CMV[3] = LIC_3();
+        CMV[4] = LIC_4();
+        CMV[5] = LIC_5();
+        CMV[6] = LIC_6();
+        CMV[7] = LIC_7();
+        CMV[8] = LIC_8();
+        CMV[9] = LIC_9();
+        CMV[10] = LIC_10();
+        CMV[11] = LIC_11();
+        CMV[12] = LIC_12();
+        CMV[13] = LIC_13();
+        CMV[14] = LIC_14();
+
+        return CMV;
+    }
+}
---
+++ src/test/java/decide/ParametersTest.java
@@ -0,0 +1,12 @@
+package decide;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ParametersTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
