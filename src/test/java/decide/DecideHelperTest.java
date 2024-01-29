5b91601a5db20f6c90da079ba784e76138f924fb
jsoderholm <soederholm.john@gmail.com>
2024-01-29 09:21:38 +0100

    Add Test Class Skeletons

---
+++ src/test/java/decide/DecideHelperTest.java
@@ -0,0 +1,12 @@
+package decide;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DecideHelperTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
