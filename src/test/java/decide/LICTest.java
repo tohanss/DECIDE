ab2c1958f0139d50107aab227fcbc4196e4b0ef3
angen <angelica.princess@hotmail.com>
2024-01-29 15:07:55 +0100

    feat: add unit-test to LIC 10

--- src/test/java/decide/LICTest.java
+++ src/test/java/decide/LICTest.java
@@ -1,12 +1,40 @@
 package decide;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class LICTest {
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LICTest extends LIC{
     @Test
     public void test() {
         assertTrue(true);
+    }
+
+    @Test
+    public void testLIC10TrueWhenPointsBetweenE_PTSAndF_PTSHaveAreaGreaterThanAREA1() {
+        final ArrayList<Point> testPoints = new ArrayList<>(Arrays.asList(new Point(1,1), new Point(1,-5), new Point(1,1), new Point(-1,-5), new Point(1,1)));
+        final int NUMPOINTS = testPoints.size();
+
+        final int AREA1 = 5;
+        final int E_PTS = 1;
+        final int F_PTS = 2;
+
+        assertTrue(LIC_10(testPoints, NUMPOINTS, AREA1, E_PTS, F_PTS));
+    }
+    @Test
+    public void testLIC10FalseWhenPointsBetweenE_PTSAndF_PTSDoesntHaveAreaGreaterThanAREA1() {
+        final ArrayList<Point> testPoints = new ArrayList<>(Arrays.asList(new Point(1,1), new Point(1,2), new Point(1,3), new Point(1,4), new Point(1,5)));
+        final int NUMPOINTS = testPoints.size();
+
+        final int AREA1 = 5;
+        final int E_PTS = 2;
+        final int F_PTS = 1;
+
+        assertFalse(LIC_10(testPoints, NUMPOINTS, AREA1, E_PTS, F_PTS));
     }
 }
