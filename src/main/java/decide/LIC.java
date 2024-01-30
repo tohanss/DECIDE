b70376e55a9ad383461e7ef4518d367210ffb72e
angen <angelica.princess@hotmail.com>
2024-01-30 19:33:39 +0100

    fix: format code according to check-style config

--- src/main/java/decide/LIC.java
+++ src/main/java/decide/LIC.java
@@ -16,30 +16,31 @@ public class LIC {
         return false;
     }
 
-    /**
-     * A method for LIC 3.
-     * There exists at least one set of three consecutive data points that are the vertices of a
-     * triangle with area greater than AREA1
-     @param points Array containing the coordinates of data points
-     @param area1 Area in LICs
-     @return true iff LIC 3 is met
-     */
-    protected boolean lic3(final ArrayList<Point> points, final int area1) {
-        Point a;
-        Point b;
-        Point c;
-        int area;
-        for (int i = 0; i < points.size() - 2; i++) {
-            a = points.get(i);
-            b = points.get(i + 1);
-            c = points.get(i + 2);
-            area = Math.abs((a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y))) / 2;
-            if (area > area1) {
-                return true;
-            }
-        }
-        return false;
+  /**
+   * A method for LIC 3.
+   * There exists at least one set of three consecutive data points that are the vertices of a
+   * triangle with area greater than AREA1
+   *
+   * @param points Array containing the coordinates of data points
+   * @param area1  Area in LICs
+   * @return true iff LIC 3 is met
+   */
+  protected boolean lic3(final ArrayList<Point> points, final int area1) {
+    Point a;
+    Point b;
+    Point c;
+    int area;
+    for (int i = 0; i < points.size() - 2; i++) {
+      a = points.get(i);
+      b = points.get(i + 1);
+      c = points.get(i + 2);
+      area = Math.abs((a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y))) / 2;
+      if (area > area1) {
+        return true;
+      }
     }
+    return false;
+  }
 
     private boolean LIC_4() {
         return false;
@@ -91,7 +92,7 @@ public class LIC {
         CMV[0] = LIC_0();
         CMV[1] = LIC_1();
         CMV[2] = LIC_2();
-        //CMV[3] = LIC_3();
+      //CMV[3] = LIC_3();
         CMV[4] = LIC_4();
         CMV[5] = LIC_5();
         CMV[6] = LIC_6();
--- src/test/java/decide/LICTest.java
+++ src/test/java/decide/LICTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import java.awt.*;
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -15,18 +15,21 @@ public class LICTest extends LIC{
         assertTrue(true);
     }
 
-    @Test
-    public void testLIC3TrueWhenPointsHaveAreaGreaterThanAREA1() {
-        final ArrayList<Point> testPoints = new ArrayList<>(Arrays.asList(new Point(1,1), new Point(1,-5), new Point(-1,-5)));
-        final int AREA1 = 5;
+  @Test
+  public void testLIC3TrueWhenPointsHaveAreaGreaterThanAREA1() {
+    final ArrayList<Point> testPoints =
+        new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(1, -5), new Point(-1, -5)));
+    final int AREA1 = 5;
 
-        assertTrue(lic3(testPoints, AREA1));
-    }
-    @Test
-    public void testLIC3FalseWhenPointsDoesntHaveAreaGreaterThanAREA1() {
-        final ArrayList<Point> testPoints = new ArrayList<>(Arrays.asList(new Point(1,1), new Point(2,2), new Point(3,3)));
-        final int AREA1 = 4;
+    assertTrue(lic3(testPoints, AREA1));
+  }
 
-        assertFalse(lic3(testPoints, AREA1));
-    }
+  @Test
+  public void testLIC3FalseWhenPointsDoesntHaveAreaGreaterThanAREA1() {
+    final ArrayList<Point> testPoints =
+        new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(2, 2), new Point(3, 3)));
+    final int AREA1 = 4;
+
+    assertFalse(lic3(testPoints, AREA1));
+  }
 }
