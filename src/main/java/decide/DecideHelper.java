e5be3e51aac90e248d1256a1f128d96c80f50435
TerenceNg03 <terenceng03@outlook.com>
2024-01-31 23:37:52 +0100

    Implement calculateFUV

--- src/main/java/decide/DecideHelper.java
+++ src/main/java/decide/DecideHelper.java
@@ -23,7 +23,6 @@ public final class DecideHelper {
     boolean[][] pum = new boolean[len][len];
     for (int i = 0; i < len; i++) {
       for (int j = 0; j < len; j++) {
-        assert (lcm[i].length == len);
         if (lcm[i][j] == "ANDD") {
           pum[i][j] = cmv[i] & cmv[j];
         } else if (lcm[i][j] == "ORR") {
@@ -34,5 +33,38 @@ public final class DecideHelper {
       }
     }
     return pum;
+  }
+
+  /**
+   * Calculate PUV based on PUM.
+   *
+   * @param pum PUM matrix
+   * @param puv PUV vector
+   * @return FUV vector
+   */
+  public static boolean[] calculateFuv(
+      final boolean[][] pum,
+      final boolean[] puv) {
+    int len = pum.length;
+    assert (puv.length == len);
+    boolean[] fuv = new boolean[len];
+    for (int i = 0; i < len; i++) {
+      if (!puv[i]) {
+        fuv[i] = true;
+        continue;
+      }
+
+      boolean row = true;
+      for (int j = 0; j < len; j++) {
+        row &= pum[i][j];
+      }
+
+      if (row) {
+        fuv[i] = true;
+      } else {
+        fuv[i] = false;
+      }
+    }
+    return fuv;
   }
 }
--- src/test/java/decide/DecideHelperTest.java
+++ src/test/java/decide/DecideHelperTest.java
@@ -13,22 +13,41 @@ public class DecideHelperTest {
    */
   @Test
   public void pumTest() {
-    boolean [] cmv = {
-      false, true, true, true, false
+    boolean[] cmv = {
+        false, true, true, true, false
     };
-    String [] [] lcm = {
-      {"ANDD", "ANDD", "ORR", "ANDD", "NOTUSED"},
-      {"ANDD", "ANDD", "ORR", "ORR", "NOTUSED"},
-      {"ORR", "ORR", "ANDD", "ANDD", "NOTUSED"},
-      {"ANDD", "ORR", "ANDD", "ANDD", "NOTUSED"},
-      {"NOTUSED", "NOTUSED", "NOTUSED", "NOTUSED", "NOTUSED"}
+    String[][] lcm = {
+        { "ANDD", "ANDD", "ORR", "ANDD", "NOTUSED" },
+        { "ANDD", "ANDD", "ORR", "ORR", "NOTUSED" },
+        { "ORR", "ORR", "ANDD", "ANDD", "NOTUSED" },
+        { "ANDD", "ORR", "ANDD", "ANDD", "NOTUSED" },
+        { "NOTUSED", "NOTUSED", "NOTUSED", "NOTUSED", "NOTUSED" }
     };
 
-    boolean [] [] pum = DecideHelper.calculatePum(cmv, lcm);
+    boolean[][] pum = DecideHelper.calculatePum(cmv, lcm);
     assertEquals(pum[0][1], false);
     assertEquals(pum[0][4], true);
     assertEquals(pum[2][1], true);
     assertEquals(pum[2][3], true);
     assertEquals(pum[3][0], false);
+  }
+
+  /**
+   * Test calculateFuv
+   */
+  @Test
+  public void fuvTest() {
+    boolean[][] pum = {
+        { true, true, false },
+        { true, true, true },
+        { true, true, true },
+    };
+    boolean[] puv = {
+        true, true, false
+    };
+    boolean[] fuv = DecideHelper.calculateFuv(pum, puv);
+    assertEquals(fuv[0], false);
+    assertEquals(fuv[1], true);
+    assertEquals(fuv[2], true);
   }
 }
