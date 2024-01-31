b01a822c6792d6b60c895be9640267014995a919
Tobias Hansson <tobihanobi@gmail.com>
2024-01-31 13:31:35 +0100

    Implement LIC 1

--- src/main/java/decide/LIC.java
+++ src/main/java/decide/LIC.java
@@ -1,14 +1,34 @@
 package decide;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
 /**
  * Class for the LIC functions.
  */
-public class Lic {
+public class LIC {
   private boolean lic0() {
     return false;
   }
 
-  private boolean lic1() {
+  /**
+   * Method for LIC 1
+   * There exists at least one set of two consecutive data points that are a distance greater than
+   * the length, LENGTH1, apart.
+   * (0 ≤ LENGTH1)
+   *
+   * @param points Array containing the coordinates of data points
+   * @param length1 Distance that two consecutive points need to be apart from each other
+   * @return True if LIC 1 is met
+   */
+  protected boolean LIC1(final ArrayList<Point> points, final double length1) {
+    for (int i = 0; i < points.size() - 1; i++) {
+      Point a = points.get(i);
+      Point b = points.get(i + 1);
+      Point distVec = new Point(a.x - b.x, a.y - b.y);
+      double dist = Math.sqrt(distVec.x * distVec.x + distVec.y * distVec.y);
+      if (dist > length1) return true;
+    }
     return false;
   }
 
@@ -74,7 +94,7 @@ public class Lic {
     boolean[] cmv = new boolean[15];
 
     cmv[0] = lic0();
-    cmv[1] = lic1();
+    //cmv[1] = LIC1();
     cmv[2] = lic2();
     cmv[3] = lic3();
     cmv[4] = lic4();
