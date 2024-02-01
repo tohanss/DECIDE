96f5d0869c2e228a867b6d5ba98ff62fa2858aba
Tobias Hansson <tobihanobi@gmail.com>
2024-02-01 18:58:45 +0100

    Implement Decide and main functions

--- src/main/java/decide/Decide.java
+++ src/main/java/decide/Decide.java
@@ -1,10 +1,27 @@
 package decide;
 
+
 /**
  * Class to the Decide problem.
  */
 public final class Decide {
-  private Decide() {
+  private Decide(String filePath) {
+    InputReader reader = new InputReader(filePath);
+
+    LIC lics = new LIC(input.getParameters(), input.getPoints(), input.getNumPoints());
+    cmv = lics.calculateCmv();
+
+    pum = DecideHelper.calculatePum(cmv, reader.getLogicalConnectorMatrix());
+
+    fuv = DecideHelper.calculateFuv(pum, reader.getPreliminaryUnlockingVector());
+
+    boolean launch = DecideHelper.calculateLaunch(fuv);
+
+    if (launch) {
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
   }
 
   /**
@@ -12,6 +29,8 @@ public final class Decide {
    * @param args command line arguments
    */
   public static void main(final String[] args) {
-    System.out.println("");
+    String filePath = args[0];
+
+    Decide decide = new Decide(filePath);
   }
 }
