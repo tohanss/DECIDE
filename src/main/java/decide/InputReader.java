89f7f9c6f8a4a426a65c57e781d0d1c42b96d13f
angen <angelica.princess@hotmail.com>
2024-02-01 20:50:22 +0100

    [doc] add documentation to getters

--- src/main/java/decide/InputReader.java
+++ src/main/java/decide/InputReader.java
@@ -10,23 +10,29 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 /**
- * Class to read the input provided from a JSON file
+ * Class to read the input provided from a JSON file.
  */
 public class InputReader {
+  /** The number of planar data points */
   private int numPoints;
+  /** Array containing the coordinates of data points */
   private ArrayList<Point2D> points;
+  /** Class holding parameters for LIC’s  */
   private Parameters parameters;
+  /** Logical Connector Matrix */
   private String[][] logicalConnectorMatrix;
+  /** Preliminary Unlocking Vector */
   private boolean[] preliminaryUnlockingVector;
 
   /**
-   * Constructor to the InputReader class
+   * Constructor to the InputReader class.
+   *
    * @param filePath the path to the JSON-file
    */
-  public InputReader(String filePath){
+  public InputReader(String filePath) {
     JSONParser jsonParser = new JSONParser();
 
-    try(FileReader reader = new FileReader(filePath)) {
+    try (FileReader reader = new FileReader(filePath)) {
       Object object = jsonParser.parse(reader);
 
       JSONObject inputVariables = (JSONObject) object;
@@ -35,7 +41,7 @@ public class InputReader {
 
       points = parsePoints((JSONArray) inputVariables.get("POINTS"));
 
-      if(numPoints != points.size()){
+      if (numPoints != points.size()) {
         throw new Exception("The amount of points and numpoints is not equal");
       }
 
@@ -43,7 +49,8 @@ public class InputReader {
 
       logicalConnectorMatrix = parseLogicalConnectorMatrix((JSONArray) inputVariables.get("LCM"));
 
-      preliminaryUnlockingVector = parsePreliminaryUnlockingVector((JSONArray) inputVariables.get("PUV"));
+      preliminaryUnlockingVector =
+          parsePreliminaryUnlockingVector((JSONArray) inputVariables.get("PUV"));
 
     } catch (IOException | ParseException e) {
       e.printStackTrace();
@@ -54,10 +61,11 @@ public class InputReader {
 
   /**
    * Method to parse the JSON-formatted array of points to correct format.
+   *
    * @param jsonPoints JSON-formatted array of points
    * @return parsed points
    */
-  private ArrayList<Point2D> parsePoints(final JSONArray jsonPoints){
+  private ArrayList<Point2D> parsePoints(final JSONArray jsonPoints) {
     ArrayList<Point2D> parsedPoints = new ArrayList<>();
     jsonPoints.forEach(point -> parsePoint(parsedPoints, (JSONObject) point));
     return parsedPoints;
@@ -65,9 +73,10 @@ public class InputReader {
 
   /**
    * Method to parse the JSON-formatted point to correct format.
+   *
    * @param jsonPoint JSON-formatted point
    */
-  private void parsePoint(ArrayList<Point2D> parsedPoints, final JSONObject jsonPoint){
+  private void parsePoint(ArrayList<Point2D> parsedPoints, final JSONObject jsonPoint) {
     final double x = ((Long) jsonPoint.get("x")).doubleValue();
     final double y = ((Long) jsonPoint.get("y")).doubleValue();
     parsedPoints.add(new Point2D.Double(x, y));
@@ -75,10 +84,11 @@ public class InputReader {
 
   /**
    * Method to parse the JSON-formatted parameters to correct format.
+   *
    * @param jsonParameters JSON-formatted parameters
    * @return parsed parameters
    */
-  private Parameters parseParameters(final JSONObject jsonParameters){
+  private Parameters parseParameters(final JSONObject jsonParameters) {
     final double length1 = ((Long) jsonParameters.get("LENGTH1")).doubleValue();
     final double radius1 = ((Long) jsonParameters.get("RADIUS1")).doubleValue();
     final double epsilon = ((Long) jsonParameters.get("EPSILON")).doubleValue();
@@ -105,16 +115,17 @@ public class InputReader {
 
   /**
    * Method to parse the JSON-formatted LCM to correct format.
+   *
    * @param jsonLogicalConnectorMatrix matrix of JSON-formatted LCM
    * @return parsed LCM
    */
-  private String[][] parseLogicalConnectorMatrix(final JSONArray jsonLogicalConnectorMatrix){
+  private String[][] parseLogicalConnectorMatrix(final JSONArray jsonLogicalConnectorMatrix) {
     assert jsonLogicalConnectorMatrix.size() == 15;
     String[][] parsedLogicalConnectorMatrix = new String[15][15];
-    for(int i = 0; i < jsonLogicalConnectorMatrix.size(); i++){
+    for (int i = 0; i < jsonLogicalConnectorMatrix.size(); i++) {
       JSONArray jsonLogicalConnectorArray = (JSONArray) jsonLogicalConnectorMatrix.get(i);
       assert jsonLogicalConnectorArray.size() == 15;
-      for(int j = 0; j < jsonLogicalConnectorArray.size(); j++){
+      for (int j = 0; j < jsonLogicalConnectorArray.size(); j++) {
         parsedLogicalConnectorMatrix[i][j] = (String) jsonLogicalConnectorArray.get(j);
       }
     }
@@ -122,35 +133,56 @@ public class InputReader {
   }
 
   /**
-   * Method to parse the JSON-formatted PUV to correct format
+   * Method to parse the JSON-formatted PUV to correct format.
+   *
    * @param jsonPreliminaryUnlockingVector array of JSON-formatted PUV
    * @return parsed PUV
    */
-  private boolean[] parsePreliminaryUnlockingVector(final JSONArray jsonPreliminaryUnlockingVector){
+  private boolean[] parsePreliminaryUnlockingVector(
+      final JSONArray jsonPreliminaryUnlockingVector) {
     boolean[] parsedPreliminaryUnlockingVector = new boolean[15];
     assert jsonPreliminaryUnlockingVector.size() == 15;
-    for(int i = 0; i < jsonPreliminaryUnlockingVector.size(); i++){
+    for (int i = 0; i < jsonPreliminaryUnlockingVector.size(); i++) {
       parsedPreliminaryUnlockingVector[i] = (boolean) jsonPreliminaryUnlockingVector.get(i);
     }
     return parsedPreliminaryUnlockingVector;
   }
 
+  /**
+   * A get-method for numPoints
+   * @return numPoints
+   */
   public int getNumPoints() {
     return numPoints;
   }
-
+  /**
+   * A get-method for points
+   * @return points
+   */
   public ArrayList<Point2D> getPoints() {
     return points;
   }
 
+  /**
+   * A get-method for parameters
+   * @return parameters
+   */
   public Parameters getParameters() {
     return parameters;
   }
 
+  /**
+   * A get-method for LCM
+   * @return LCM
+   */
   public String[][] getLogicalConnectorMatrix() {
     return logicalConnectorMatrix;
   }
 
+  /**
+   * A get-method for PUV
+   * @return PUV
+   */
   public boolean[] getPreliminaryUnlockingVector() {
     return preliminaryUnlockingVector;
   }
